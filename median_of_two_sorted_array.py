5f2666b51aa4d6bd347145c5ee0c2ac4d1c4b021
Khan Asfi Reza <khanasfireza10@gmail.com>
2021-09-03 02:03:09 +0600

    3. Median of two sorted arrays

---
+++ median_of_two_sorted_array.py
@@ -0,0 +1,37 @@
+def findMedianSortedArrays(nums1, nums2):
+    n = len(nums1)
+    m = len(nums2)
+
+    if len(nums2) > len(nums1):
+        nums1, nums2 = nums2, nums1
+
+    k = n + m
+    is_even = False
+
+    if k % 2 != 0:
+        length = (k + 1) // 2
+    else:
+        length = (k // 2) + 1
+        is_even = True
+
+    i = 0
+    j = 0
+    new_list = []
+
+    for o in range(length):
+        if i < n:
+            if j < m:
+                if nums1[i] <= nums2[j]:
+                    new_list.append(nums1[i])
+                    i += 1
+                else:
+                    new_list.append(nums2[j])
+                    j += 1
+            else:
+                new_list.append(nums1[i])
+                i += 1
+        else:
+            new_list.append(nums2[j])
+            j += 1
+
+    return (new_list[length - 1] + new_list[length - 2]) / 2 if is_even else new_list[length - 1]
